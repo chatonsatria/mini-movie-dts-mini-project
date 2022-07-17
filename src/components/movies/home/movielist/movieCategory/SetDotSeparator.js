3e913126ab5fe74587513550e5f1fda5abab4b5e
chatonsatria <martin1998.mcsy@gmail.com>
2022-07-18 02:35:19 +0700

    [add function] - setDotInterval and setMovieCategory

---
+++ src/components/movies/home/movielist/movieCategory/SetDotSeparator.js
@@ -0,0 +1,7 @@
+import { setCategory } from "./MovieCategory";
+
+export function setDotInterval(dataIds) {
+  let dataIdsArray = [];
+  dataIds.forEach((data) => dataIdsArray.push(setCategory(data)));
+  return dataIdsArray.join(" · ");
+}
