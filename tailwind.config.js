97db4b6380746930e85aa779a0c2303ad679cc02
chatonsatria <martin1998.mcsy@gmail.com>
2022-07-19 02:41:43 +0700

    [tailwind config] - add animated style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      animation: {
+        "scale-cycle": "scale_cycle 2s linear infinite",
+      },
+      keyframes: {
+        scale_cycle: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
+        },
+      },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
